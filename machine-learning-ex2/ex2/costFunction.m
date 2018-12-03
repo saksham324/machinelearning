e9232a30dbae88376795355576092a3a6f93fac4
saksham324 <saksham324@gmail.com>
2018-12-03 11:46:12 +0530

    Add files via upload

---
+++ machine-learning-ex2/ex2/costFunction.m
@@ -0,0 +1,32 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+
+hypothesis = sigmoid(X*theta); 
+sigma = sum(y.*log(hypothesis))+sum((ones(size(y))-y).*log(ones(size(hypothesis))-(hypothesis)));
+J = -(1/m)*(sigma); 
+grad = (1/m)*sum((hypothesis-y).*X);
+
+
+
+% =============================================================
+
+end
